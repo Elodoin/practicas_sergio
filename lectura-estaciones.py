4b1a95b7cc0a4c967f6dfde5e9ed7e5cd908a1f3
sergi1307 <sergiocatalan98@gmail.com>
2021-03-25 17:31:46 +0100

    lectura-estaciones def
    Pequeña corrección para que Linux pueda leer el programa

--- lectura-estaciones.py
+++ lectura-estaciones.py
@@ -1,9 +1,8 @@
 #Aqui ira el programa que lea el fichero de estaciones
 
-
 import pylab as py
-islas_x,islas_y = py.loadtxt("utils_sergio\\contorno_islas.txt", unpack=True)
-est_y,est_x=py.loadtxt("utils_sergio\\fichero_estaciones.txt",comments='---',usecols=(4,5),unpack=True)
+islas_x,islas_y = py.loadtxt("utils_sergio//contorno_islas.txt", unpack=True)
+est_y,est_x=py.loadtxt("utils_sergio//fichero_estaciones.txt",comments='---',usecols=(4,5),unpack=True)
 
 py.ion()
 py.plot(islas_x,islas_y,',')     
