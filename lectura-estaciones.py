8520c25f0e3e2e85115fe430af4e2251c57bd0b4
sergi1307 <sergiocatalan98@gmail.com>
2021-03-25 10:15:08 +0100

    Update lectura-estaciones.py
    Versión 1

--- lectura-estaciones.py
+++ lectura-estaciones.py
@@ -5,7 +5,6 @@ import pylab as py
 islas_x,islas_y = py.loadtxt("C:\\Users\\Sergio Catalán\\Documents\\GitHub\\practicas_sergio\\utils_sergio\\contorno_islas.txt", unpack=True)
 est_y,est_x=py.loadtxt("C:\\Users\\Sergio Catalán\\Documents\\GitHub\\practicas_sergio\\utils_sergio\\fichero_estaciones.txt",comments='---',usecols=(3,4),unpack=True)
 
-
 py.ion()
 py.plot(islas_x,islas_y,',')     
 py.plot(est_x,est_y,'.','r')    
