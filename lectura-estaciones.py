9e38b487cf81bf2d9dcd502151d96ca4cc6c9cf6
sergi1307 <sergiocatalan98@gmail.com>
2021-03-29 16:35:07 +0200

    lectura-estaciones funcionalizada
    Añadida la representación gráfica dentro de una función

--- lectura-estaciones.py
+++ lectura-estaciones.py
@@ -1,13 +1,20 @@
 #Aqui ira el programa que lea el fichero de estaciones
 
-import pylab as py
-islas_x,islas_y = py.loadtxt("utils_sergio//contorno_islas.txt", unpack=True)
-est_y,est_x=py.loadtxt("utils_sergio//fichero_estaciones.txt",comments='---',usecols=(4,5),unpack=True)
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pa
 
-py.ion()
-py.plot(islas_x,islas_y,',')     
-py.plot(est_x,est_y,'.')
-py.xlabel('Longitud')
-py.ylabel('Latitud')    
-py.grid()
-py.show()
+#Extraemos de los ficheros las posiciones de las islas y de las esatciones sismicas
+islas_x,islas_y = np.loadtxt("utils_sergio//contorno_islas.txt", unpack=True)
+est_y,est_x=np.loadtxt("utils_sergio//fichero_estaciones.txt",comments='---',usecols=(4,5),unpack=True)
+
+#Definimos la funcion mediante la que representaremos nuestra imagen
+def representacion(islas_x,islas_y,est_x,est_y):
+    plt.plot(islas_x,islas_y,',')     
+    plt.plot(est_x,est_y,'.')
+    plt.xlabel('Longitud')
+    plt.ylabel('Latitud')    
+    plt.grid()
+    plt.show()
+    
+representacion(islas_x,islas_y,est_x,est_y)    
